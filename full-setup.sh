66839ec2aa42f2977d5a3aa3c96cf90302128306
EPAPRA <dave.devops109@gmail.com>
2023-07-01 16:34:57 +0530

    multibranch commit

---
+++ full-setup.sh
@@ -0,0 +1,5 @@
+for i in frontend cart catalogue user shipping payment redis mongodb mysql rabbitmq ; do
+
+  ansible-playbook -i ${component}.davedevops.tech, roboshop.yml -e ansible_user=centos -e ansible_password=DevOps321 -e ROLE_NAME=${component} -e env=dev
+
+done
